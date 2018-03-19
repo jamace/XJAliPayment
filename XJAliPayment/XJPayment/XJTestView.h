d70d0bc8c366c5e6055ce15a12ab471772b9abc2
Your Name Here <you@yourdomain.com>
2018-03-19 15:55:36 +0800

    hhh

---
+++ XJAliPayment/XJPayment/XJTestView.h
@@ -0,0 +1,13 @@
+//
+//  XJTestView.h
+//  XJAliPayment
+//
+//  Created by 肖吉 on 2018/3/19.
+//  Copyright © 2018年 jamace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XJTestView : UIView
+
+@end
---
+++ XJAliPayment/XJPayment/XJTestView.m
@@ -0,0 +1,21 @@
+//
+//  XJTestView.m
+//  XJAliPayment
+//
+//  Created by 肖吉 on 2018/3/19.
+//  Copyright © 2018年 jamace. All rights reserved.
+//
+
+#import "XJTestView.h"
+
+@implementation XJTestView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
